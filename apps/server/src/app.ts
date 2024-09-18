0d8d747d08c0d583eabfea0d7591ba051a174581
Sam Bender <2336186+rednebmas@users.noreply.github.com>
2024-09-18 10:24:08 -0700

    eslint attempt & seed db

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -82,9 +82,9 @@ const initApp = async () => {
 			// const { tritoneSubRhDeck, tritoneSubRhCards, tritoneSubLhDeck, tritoneSubLHCards } =
 			// 	singleHandedTritoneSub251('x');
 			// replayAttemptsForUser('66d4a69b871f809f48a0d2b7');
-			// generate251s();
-			// generateBluesCourse();
-			// generatePopCourse();
+			generate251s();
+			generateBluesCourse();
+			generatePopCourse();
 		});
 	} catch (error: any) {
 		console.error(`Error:${error.message}`);
