6a0d907cb7650e0105c4216d4ea5958d1316468f
Sam Bender <2336186+rednebmas@users.noreply.github.com>
2024-09-25 23:30:58 -0700

    iOS app initial commit

---
+++ apps/ios/MemoryFlashiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryFlashiOS
+//
+//  Created by Sam Bender on 9/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ apps/ios/MemoryFlashiOSTests/MemoryFlashiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryFlashiOSTests.swift
+//  MemoryFlashiOSTests
+//
+//  Created by Sam Bender on 9/25/24.
+//
+
+import Testing
+@testable import MemoryFlashiOS
+
+struct MemoryFlashiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
