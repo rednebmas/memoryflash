01a0a9d5b53df660e91ac6564d4ffcd33e832843
Sam Bender <rednebmas@users.noreply.github.com>
2025-06-03 22:36:24 -0700

    fix: correct oneWeekInSeconds constant (#7)

--- apps/server/src/utils/dates.ts
+++ apps/server/src/utils/dates.ts
@@ -1,7 +1,7 @@
 export const oneMinInSeconds = 60;
 export const oneHourInSeconds = oneMinInSeconds * 60;
 export const oneDayInSeconds = oneHourInSeconds * 24;
-export const oneWeekInSeconds = oneHourInSeconds * 24;
+export const oneWeekInSeconds = oneDayInSeconds * 7;
 export const oneMonthInSeconds = oneDayInSeconds * 30;
 
 export const oneMinInMillis = 1000 * 60;
