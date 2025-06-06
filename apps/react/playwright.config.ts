4c46a271679f836cbd78fe59b3ca657aaedfbaa4
Sam Bender <rednebmas@users.noreply.github.com>
2025-06-05 23:36:17 -0700

    Use same screenshot across platforms

--- apps/react/playwright.config.ts
+++ apps/react/playwright.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:4173',
     screenshot: 'only-on-failure',
+    snapshotPathTemplate: '{testDir}/{testFilePath}-snapshots/{arg}{ext}',
   },
   reporter: [['html', { open: 'never' }]],
 });
