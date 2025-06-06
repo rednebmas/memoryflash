3d453c8b3b27ab11f42781f403a79dd846d8c0e0
Sam Bender <rednebmas@users.noreply.github.com>
2025-06-05 23:30:21 -0700

    style: apply tailwind sign-in form

--- apps/react/src/screens/LoginScreen.tsx
+++ apps/react/src/screens/LoginScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { login } from 'MemoryFlashCore/src/redux/actions/login-action';
 import { useAppDispatch } from 'MemoryFlashCore/src/redux/store';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useNetworkState } from 'MemoryFlashCore/src/redux/selectors/useNetworkState';
 import { useUpdateEffect } from '../utils/useUpdateEffect';
 
@@ -28,32 +28,87 @@ export const LoginScreen: React.FunctionComponent<{}> = ({}) => {
 		dispatch(login(email, password));
 	};
 
-	return (
-		<div>
-			<h1>Log in</h1>
-			<form onSubmit={handleSubmit}>
-				<input
-					type='text'
-					placeholder='Email'
-					value={email}
-					onChange={(e) => {
-						setEmail(e.target.value);
-					}}
-				/>
-				<input
-					type='password'
-					placeholder='Password'
-					value={password}
-					onChange={(e) => {
-						setPassword(e.target.value);
-					}}
-				/>
-				<div className='text-red-600 '>{error}</div>
+        return (
+                <div className='flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8'>
+                        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+                                <h2 className='mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+                                        Sign in to your account
+                                </h2>
+                        </div>
 
-				<button type='submit' className=''>
-					Login
-				</button>
-			</form>
-		</div>
-	);
+                        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+                                <div className='bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12'>
+                                        <form className='space-y-6' onSubmit={handleSubmit}>
+                                                <div>
+                                                        <label htmlFor='email' className='block text-sm font-medium leading-6 text-gray-900'>
+                                                                Email address
+                                                        </label>
+                                                        <div className='mt-2'>
+                                                                <input
+                                                                        id='email'
+                                                                        type='email'
+                                                                        value={email}
+                                                                        onChange={(e) => setEmail(e.target.value)}
+                                                                        required
+                                                                        autoComplete='email'
+                                                                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
+                                                                />
+                                                        </div>
+                                                </div>
+
+                                                <div>
+                                                        <label htmlFor='password' className='block text-sm font-medium leading-6 text-gray-900'>
+                                                                Password
+                                                        </label>
+                                                        <div className='mt-2'>
+                                                                <input
+                                                                        id='password'
+                                                                        type='password'
+                                                                        value={password}
+                                                                        onChange={(e) => setPassword(e.target.value)}
+                                                                        required
+                                                                        autoComplete='current-password'
+                                                                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6'
+                                                                />
+                                                        </div>
+                                                </div>
+
+                                                <div className='flex items-center justify-between'>
+                                                        <div className='flex items-center'>
+                                                                <input
+                                                                        id='remember-me'
+                                                                        type='checkbox'
+                                                                        className='h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-600'
+                                                                        defaultChecked
+                                                                />
+                                                                <label htmlFor='remember-me' className='ml-3 block text-sm leading-6 text-gray-900'>
+                                                                        Remember me
+                                                                </label>
+                                                        </div>
+                                                        <div className='text-sm leading-6'>
+                                                                <a href='#' className='font-semibold text-blue-600 hover:text-blue-500'>
+                                                                        Forgot password?
+                                                                </a>
+                                                        </div>
+                                                </div>
+
+                                                <div className='text-red-600'>{error}</div>
+
+                                                <button
+                                                        type='submit'
+                                                        className='flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
+                                                >
+                                                        Sign in
+                                                </button>
+                                        </form>
+                                        <p className='mt-10 text-center text-sm text-gray-500'>
+                                                Not a member?{' '}
+                                                <Link to='/auth/sign-up' className='font-semibold leading-6 text-blue-600 hover:text-blue-500'>
+                                                        Sign up
+                                                </Link>
+                                        </p>
+                                </div>
+                        </div>
+                </div>
+        );
 };
