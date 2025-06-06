a704391b32ee538c5659c2424c2b99f5942bed26
Sam Bender <2336186+rednebmas@users.noreply.github.com>
2025-06-05 22:09:16 -0700

    Commit correct image and add to git ignore

--- apps/react/src/music-notation-test.tsx
+++ apps/react/src/music-notation-test.tsx
@@ -9,24 +9,32 @@ import { StaffEnum } from 'MemoryFlashCore/src/types/Cards';
 const store = createStore({ scheduler: { multiPartCardIndex: 0 } } as any, () => {});
 
 const data: MultiSheetQuestion = {
-  key: 'C',
-  voices: [
-    {
-      staff: StaffEnum.Treble,
-      stack: [
-        { notes: [{ name: 'C', octave: 4 }], duration: 'q', chordName: 'C' },
-        { notes: [{ name: 'D', octave: 4 }], duration: 'q' },
-        { notes: [{ name: 'E', octave: 4 }], duration: 'q' },
-        { notes: [{ name: 'F', octave: 4 }], duration: 'q' },
-      ],
-    },
-  ],
+	key: 'C',
+	voices: [
+		{
+			staff: StaffEnum.Treble,
+			stack: [
+				{
+					notes: [
+						{ name: 'C', octave: 4 },
+						{ name: 'E', octave: 4 },
+						{ name: 'G', octave: 4 },
+					],
+					duration: 'q',
+					chordName: 'C',
+				},
+				{ notes: [{ name: 'D', octave: 4 }], duration: 'q' },
+				{ notes: [{ name: 'E', octave: 4 }], duration: 'q' },
+				{ notes: [{ name: 'F', octave: 4 }], duration: 'q' },
+			],
+		},
+	],
 };
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <MusicNotation data={data} />
-    </Provider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Provider store={store}>
+			<MusicNotation data={data} />
+		</Provider>
+	</React.StrictMode>,
 );
