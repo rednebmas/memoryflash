d40ee61c5e747d97d941f268abd8146b636ad74f
Sam Bender <rednebmas@users.noreply.github.com>
2025-06-06 20:34:05 -0700

    Fix MusicNotation strict voice and add MusicRecorder screenshot test

---
+++ apps/react/src/music-recorder-test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { MusicRecorder } from './components/MusicRecorder';
+import { createStore } from 'MemoryFlashCore/src/redux/store';
+
+const store = createStore({ scheduler: { multiPartCardIndex: 0 } } as any, () => {});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+        <React.StrictMode>
+                <Provider store={store}>
+                        <MusicRecorder />
+                </Provider>
+        </React.StrictMode>,
+);
