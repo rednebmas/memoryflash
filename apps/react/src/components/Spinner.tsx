c8f8d54bd72325c172f40aa61580a0d0d8df88fc
Sam Bender <rednebmas@users.noreply.github.com>
2025-06-06 21:12:31 -0700

    Add spinner loading state on auth pages (#16)
    * Add loading prop to Button for built-in spinner
    
    * fix: adjust spinner color for dark mode
    
    * chore: update spinner color
    
    * fixes
    
    ---------
    
    Co-authored-by: Sam Bender <2336186+rednebmas@users.noreply.github.com>

--- apps/react/src/components/Spinner.tsx
+++ apps/react/src/components/Spinner.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 interface SpinnerProps {
 	show?: boolean;
+	className?: string;
+	svgClassName?: string;
 }
 
-export const Spinner: React.FunctionComponent<SpinnerProps> = ({ show = true }) => {
+export const Spinner: React.FunctionComponent<SpinnerProps> = ({
+	show = true,
+	className = '',
+	svgClassName = 'w-8 h-8',
+}) => {
 	if (!show) return null;
 	return (
-		<div className="flex justify-center" role="status">
+		<div className={`flex justify-center ${className}`} role="status">
 			<svg
 				aria-hidden="true"
-				className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+				className={`text-gray-400 fill-blue-600 animate-spin ${svgClassName}`}
 				viewBox="0 0 100 101"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
