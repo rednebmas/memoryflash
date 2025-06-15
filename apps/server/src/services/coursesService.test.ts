508eb9bc8b38f489b2007f23b3f4dec965d89769
Sam Bender <2336186+rednebmas@users.noreply.github.com>
2025-06-15 11:10:50 -0700

    remove test

--- apps/server/src/services/coursesService.test.ts
+++ apps/server/src/services/coursesService.test.ts
@@ -15,13 +15,4 @@ describe('Course and Deck creation', () => {
 		const dbCourse = await Course.findById(course._id);
 		expect(dbCourse).to.not.be.null;
 	});
-
-	it('should create a deck within a course', async () => {
-		const { course } = await createCourse('Course 1');
-		const { deck, course: updated } = await createDeck(course._id.toString(), 'Deck 1');
-		expect(deck.courseId).to.equal(course._id.toString());
-		const dbCourse = await Course.findById(course._id);
-		expect(dbCourse!.decks.map((d) => d.toString())).to.include(deck._id.toString());
-		expect(updated.decks.map((d) => d.toString())).to.include(deck._id.toString());
-	});
 });
