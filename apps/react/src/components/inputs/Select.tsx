0d4032796c030e94158543f4956b8228867a1036
Sam Bender <rednebmas@users.noreply.github.com>
2025-06-16 00:54:43 -0700

    fix select styling for dark mode (#50)

--- apps/react/src/components/inputs/Select.tsx
+++ apps/react/src/components/inputs/Select.tsx
@@ -4,7 +4,11 @@ export type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement>;
 
 export const Select = React.forwardRef<HTMLSelectElement, SelectProps>(
 	({ className = '', children, ...props }, ref) => (
-		<select ref={ref} className={`form-select rounded ${className}`} {...props}>
+		<select
+			ref={ref}
+			className={`block w-full rounded-md border-0 py-1.5 bg-white dark:bg-gray-800 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 ${className}`}
+			{...props}
+		>
 			{children}
 		</select>
 	),
