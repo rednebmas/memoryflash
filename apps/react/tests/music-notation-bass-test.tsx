eb6ac3c67cac49f3b328e5b697892d4411234ee3
Sam Bender <rednebmas@users.noreply.github.com>
2025-06-18 12:27:29 -0700

    test: add MusicNotation bass clef spec

---
+++ apps/react/tests/music-notation-bass-test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MusicNotation } from '../src/components/MusicNotation';
+import '../src/index.css';
+import { MultiSheetQuestion } from 'MemoryFlashCore/src/types/MultiSheetCard';
+import { StaffEnum } from 'MemoryFlashCore/src/types/Cards';
+import { renderApp } from './renderApp';
+
+const data: MultiSheetQuestion = {
+	key: 'C',
+	voices: [
+		{
+			staff: StaffEnum.Bass,
+			stack: [
+				{ notes: [{ name: 'C', octave: 2 }], duration: 'q' },
+				{ notes: [{ name: 'D', octave: 2 }], duration: 'q' },
+				{ notes: [{ name: 'E', octave: 2 }], duration: 'q' },
+				{ notes: [{ name: 'F', octave: 2 }], duration: 'q' },
+			],
+		},
+	],
+};
+
+renderApp(<MusicNotation data={data} highlightClassName="highlight" />);
