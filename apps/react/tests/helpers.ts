a5465b17e5ee99dc8b4e7cd8686cf2e2241e09d7
Sam Bender <rednebmas@users.noreply.github.com>
2025-06-18 10:54:43 -0700

    Block fonts during screenshot tests (#100)
    * test: control font downloads via env var
    
    * rename
    
    ---------
    
    Co-authored-by: Sam Bender <2336186+rednebmas@users.noreply.github.com>

--- apps/react/tests/helpers.ts
+++ apps/react/tests/helpers.ts
@@ -1,1 +1,18 @@
+import { test as base, expect, Page } from '@playwright/test';
+
 export const screenshotOpts = { maxDiffPixelRatio: 0.015 };
+
+// If the font does/doesn't load can cause small differences making tests more flaky
+const blockFonts = process.env.BLOCK_REMOTE_FONTS === 'true';
+
+export const test = base.extend<{ page: Page }>({
+	page: async ({ page }, use) => {
+		if (blockFonts) {
+			await page.route('https://fonts.googleapis.com/**', (route) => route.abort());
+			await page.route('https://fonts.gstatic.com/**', (route) => route.abort());
+		}
+		await use(page);
+	},
+});
+
+export { expect };
--- apps/react/tests/music-notation-highlight.spec.ts
+++ apps/react/tests/music-notation-highlight.spec.ts
@@ -1,5 +1,4 @@
-import { test, expect } from '@playwright/test';
-import { screenshotOpts } from './helpers';
+import { test, expect, screenshotOpts } from './helpers';
 
 test.describe('highlight notes', () => {
 	for (const index of [1, 2, 3, 4]) {
