89b7127a5cf02078e4ada9936dbf68df0a13400b
Sam Bender <2336186+rednebmas@users.noreply.github.com>
2025-06-18 14:08:33 -0700

    rename and delete

--- apps/react/src/components/SectionCard.tsx
+++ apps/react/src/components/SectionCard.tsx
@@ -25,7 +25,7 @@ export const SectionCard: React.FC<SectionCardProps> = ({
 			contentContainerClassName="justify-between items-center relative"
 		>
 			{menu && (
-				<div className="absolute -top-2 -right-2 sm:opacity-0 sm:group-hover:opacity-100">
+				<div className="absolute top-2 right-2 sm:opacity-0 sm:group-hover:opacity-100">
 					{menu}
 				</div>
 			)}
@@ -33,6 +33,7 @@ export const SectionCard: React.FC<SectionCardProps> = ({
 				<div className="text-sm font-medium">{title}</div>
 				{subTitle && <div className="text-[0.6rem]">{subTitle}</div>}
 			</div>
+
 			<Link className="w-full" to={link}>
 				<button className="rounded-md w-full bg-blue-500 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ">
 					{btnText}
